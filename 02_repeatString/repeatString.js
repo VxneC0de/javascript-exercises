9dffbe857b3cdce279329aebd7705fbffda78d7b
VxneC0de <vanerubio2020@gmail.com>
2024-06-14 16:46:41 -0400

    feat: finished second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000);
 
+const repeatString = function(string, num) {
+  let word = string;
+  let array = "";
+  let count = 0;
+  
+
+  if(num < 0){
+    return 'ERROR';
+  }
+
+  while(count < num){
+    array += word;
+    count++;
+  }
+  return array;
 };
+
+repeatString("", number);
 
 // Do not edit below this line
 module.exports = repeatString;
