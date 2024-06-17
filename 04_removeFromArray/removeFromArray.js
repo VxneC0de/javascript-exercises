ea91d4b637b35a0c4ac64a386bcc8ce50914bd88
VxneC0de <vanerubio2020@gmail.com>
2024-06-17 16:40:42 -0400

    feat: continue removeFromArray test 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for(let i =0; i < elements.length; i++){
+        const index = array.indexOf(elements[i]);
+        if(index > -1){
+            array.splice(index, 1);
+        }
+    };
+
+    return array;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
