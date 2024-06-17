760bdb6cc16fe85d502eb950f8c4c215ab61c8e0
yorley2024 <yorlemartinez26@gmail.com>
2024-06-17 15:25:13 -0400

    feat: finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let expression = word.split("").reverse().join("");
+    return expression;
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
