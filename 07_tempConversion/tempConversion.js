b776698cf99b3cccf2cb2a230c7fbd455df5756c
VxneC0de <vanerubio2020@gmail.com>
2024-06-23 16:20:41 -0400

    feat: finished exercise  tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  return parseFloat(celsius.toFixed(1)); // Redondeamos a un decimal
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius();
+
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  return parseFloat(fahrenheit.toFixed(1)); // Redondeamos a un decimal
 };
+
+convertToFahrenheit();
 
 // Do not edit below this line
 module.exports = {
