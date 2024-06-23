c2dc9694bcddfa5ca978c299d3d4ba8d264761fa
VxneC0de <vanerubio2020@gmail.com>
2024-06-23 15:36:54 -0400

    feat: finished exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
+console.log(sumAll(1, 4));
+
+/*
+La función Math.min(a, b) devuelve el número más pequeño entre a y b
+Math.max(a, b) devuelve el número más grande
+*/
+
+/*
+El operador typeof en JavaScript devuelve una cadena que indica el tipo del valor de la expresión o variable sin evaluarla. Es útil para verificar el tipo de una variable en tu código.
+typeof "John" devuelve "string".
+typeof 3.14 devuelve "number".
+typeof true devuelve "boolean".
+*/
+
 // Do not edit below this line
 module.exports = sumAll;
+
+
